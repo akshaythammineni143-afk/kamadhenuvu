3395c3f96fcd42aa46179a7c954a77fab5fe6ea5
Akshay <akshay@example.com>
2026-07-06 12:54:11 +0530

    Configure static Supabase credentials in config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // If you host this website online, you can paste your Supabase credentials here 
 // so that all devices (including guest mobile phones) can connect automatically without configuration.
 
-window.CONFIG_SB_URL = "";
-window.CONFIG_SB_ANON = "";
+window.CONFIG_SB_URL = "https://wynxbflrzympicpgxeas.supabase.co";
+window.CONFIG_SB_ANON = "sb_publishable_gSGImre9tHBmbfvXoMc2YQ_w5Dc2HNL";
